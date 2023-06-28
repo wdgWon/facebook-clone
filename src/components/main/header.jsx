e392a291e2f449381a0ab39dccd162ad8b4c7c7d
kang taehyun <63766246+kwm2546@users.noreply.github.com>
2023-06-29 01:14:58 +0900

    0629

--- src/components/main/header.jsx
+++ src/components/main/header.jsx
@@ -7,41 +7,48 @@ import Modals from "./promodals";
 const Header = () => {
     return (
         <header className="flex h-[56px]">
-            <div className="flex flex-just item-center">
-                <img className="w-[50px]" src={fblogo} alt="페이스북로고" />
+            <div className="flex items-center">
+                <a href="/">
+                    <img
+                        className="w-[50px]"
+                        src={fblogo}
+                        alt="페이스북 로고"
+                    />
+                </a>
                 <SearchBar />
             </div>
-            {/*네비바*/}
-            <div className="">
-                <a href="/" className="hover:bg-gray-200">
-                    홈
-                </a>
-                <a href="/watch" className="hover:bg-gray-200">
-                    워치
-                </a>
-                <a href="/" className="hover:bg-gray-200">
-                    그룹
-                </a>
-                <a href="/" className="hover:bg-gray-200">
-                    게임
-                </a>
-            </div>
 
-            <div className="">
-                <a href="" className="hover:bg-gray-200">
-                    +
-                </a>
-                <a href="" className="hover:bg-gray-200">
-                    메세지
-                </a>
-                <a href="" className="hover:bg-gray-200">
-                    알람
-                </a>
+            <div className="flex items-center">
+                <div className="flex justify-center p-4">
+                    <a href="/" className="hover:bg-gray-200">
+                        홈
+                    </a>
+                    <a href="/watch" className="hover:bg-gray-200">
+                        워치
+                    </a>
+                    <a href="/group" className="hover:bg-gray-200">
+                        그룹
+                    </a>
+                    <a href="/game" className="hover:bg-gray-200">
+                        게임
+                    </a>
+                </div>
+
+                <div className="p-4">
+                    <a href="/plus?" className="hover:bg-gray-200">
+                        +
+                    </a>
+                    <a href="/message" className="hover:bg-gray-200">
+                        메세지
+                    </a>
+                    <a href="/notify" className="hover:bg-gray-200">
+                        알람
+                    </a>  
+                    <a href="/profiles" className="hover:bg-gray-200">
+                        프로필
+                    </a>
+                </div>
             </div>
-            {/*프로필창모달을 하고싶다..*/}
-            <a href="/profiles" className="hover:bg-gray-200">
-                프로필
-            </a>
         </header>
     );
 };
