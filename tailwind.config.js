29b75591b6c1a44bbe1c1b06d8a72b4ee9824eb2
wdgWon <same666@naver.com>
2023-07-03 13:53:31 +0900

    Fix: 테일윈드 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 export default {
   content: [" ./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    width: {
-      1250: "1250px",
-    },
-    height: {
-      460: "460px",
-    },
     extend: {},
   },
   plugins: [],
